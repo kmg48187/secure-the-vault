239d645df09244136e18114bd59e97f0906846c5
Kristen George <kmstrumpfer@yahoo.com>
2025-03-03 21:08:27 -0500

    secure the vault alert

---
+++ secure-the-vault.js
@@ -0,0 +1,13 @@
+//I need to write three different equations to equal the totals 
+// 10 - 40 - 39. 
+
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret code:";
+let num1 = 5*2;
+let num2 = 50-10;
+let num3 = 30+9;
+//expected output of code: 10 - 40 - 39 
+//now I just need an alert containing all my variables
+//alert = (`${message} ${num1} - ${num2} - ${num3}`); 
+//console.log(alert);
+//I actually made a value named "alert"
+alert(`${message} ${num1} - ${num2} - ${num3}`);
